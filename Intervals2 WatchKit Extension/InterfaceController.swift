21473cf15f662a373c97ad8a8f6fa1ee29a67007
Ole Friis Østergaard <ole.ostergaard@microsoft.com>
2019-03-06 12:53:44 +0100

    Initial Commit

---
+++ Intervals2 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Intervals2 WatchKit Extension
+//
+//  Created by Ole Friis Østergaard on 06/03/2019.
+//  Copyright © 2019 SpikeSoft. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Intervals2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Intervals2
+//
+//  Created by Ole Friis Østergaard on 06/03/2019.
+//  Copyright © 2019 SpikeSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
